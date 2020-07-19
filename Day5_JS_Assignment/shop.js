7659afb3f0c894842f3004669063b3a46e30d875
Praveen Kumar <neevarp1729@gmail.com>
2020-07-19 21:14:38 +0530

    Adding Day 5 Assignments

---
+++ Day5_JS_Assignment/shop.js
@@ -0,0 +1,7 @@
+console.log("I am going for shopping");
+shoppingList = ["Milk","Sugar","Rice","Chocolates"];
+console.log(shoppingList);
+let shoppingBasket = [...shoppingList];
+console.log(shoppingBasket);
+shoppingList.push("Candies");
+console.log(shoppingList);
